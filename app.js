1f65e20464a5a36099d87a696d4e30ff4a141e53
silviofbarbieri <silviofbarbieri@gmail.com>
2024-10-24 10:15:34 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+// Wait for the document to be ready
+document.addEventListener('DOMContentLoaded', function () {
+  // Create a map instance and set the initial view coordinates and zoom level
+  var map = L.map('map').setView([-23.517893, -46.834978], 13);
+
+
+  // Add a tile layer to the map from OpenStreetMap
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+
+  // Create a marker with popup and add it to the map
+  var marker = L.marker([-23.517893, -46.834978]).addTo(map);
+  marker.bindPopup("Amada Turma de DMD, nosso projeto salve o dog!").openPopup();
+});
