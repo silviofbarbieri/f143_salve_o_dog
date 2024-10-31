dafcd8ee06f3ebade5542ab55ec4ae2f24e3b2cf
silviofbarbieri <silviofbarbieri@gmail.com>
2024-10-31 10:18:27 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ import Store from './Store';
 import { MapsComponent } from './mapsComponent';
 import { CARD_WIDTH } from './Store';
 
+// Wait for the document to be ready
+document.addEventListener('DOMContentLoaded', function () {
+  // Create a map instance and set the initial view coordinates and zoom level
+  var map = L.map('map').setView([51.505, -0.09], 13);
+
+
+  // Add a tile layer to the map from OpenStreetMap
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+
 const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
 
 const Locations = [
